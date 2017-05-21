288cf6a78a8960a65445952a63a9ace419cf312c
Mitra <bhanumchandra@gmail.com>
2017-05-21 16:05:35 -0500

    Implementing findPosition functionality, which gives the position of a site in the n by n grid

--- src/main/java/algorithm/Percolation.java
+++ src/main/java/algorithm/Percolation.java
@@ -8,16 +8,20 @@ import java.util.stream.IntStream;
  * @author MITRA
  */
 public class Percolation {
-
+    
+    private final int n;
     private final int size;
-    private int[] status;
+    private final int[] status;
     private final int TOP;
     private final int BOT;
     private final WeightedQuickUnionUF uf;
+    //positions of a site  in the 2D grid
+    private enum POSITION {LeftTopCorner, RightTopCorner, LeftBotCorner, RightBotCorner, Top, Bot, Left, Right, Middle};
 
     // create n-by-n grid, with all sites blocked
     public Percolation(int n) {
         verify(n);
+        this.n = n;
         this.size = n * n + 2;
         this.TOP = size - 2;
         this.BOT = size - 1;
@@ -39,8 +43,30 @@ public class Percolation {
         validate(col);
         int index = calculateIndex(row, col);
         status[index] = 1;
+        performUnion(row, col);
     }
 
+    private void performUnion(int row, int col) {
+        
+        POSITION position = findPosition(row, col);
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private POSITION findPosition(int row, int col) {
+        POSITION x = POSITION.Middle;
+        int index = calculateIndex(row, col);
+        if(index == 1){ x = POSITION.LeftTopCorner;}
+        else if( index == n) { x = POSITION.RightTopCorner;}
+        else if( index == size - 2) { x = POSITION.RightBotCorner;}
+        else if( index == size - 1 - n) {x = POSITION.LeftBotCorner;}
+        else if((index % n) == 1 ) { x = POSITION.Left;}
+        else if((index % n) == 0 ) { x = POSITION.Right;}
+        else if((index - n ) < 0) { x = POSITION.Top;}
+        else if((index + n) > size -2){x = POSITION.Bot;}
+        return x;
+    }
+    
+    
     // is site (row, col) open?
     public boolean isOpen(int row, int col) {
         validate(row);
@@ -68,8 +94,8 @@ public class Percolation {
         return true;
     }
 
-    private void validate(int index) {
-        if (index < 1 || index > size) {
+    private void validate(int x) {
+        if (x < 1 || x > size) {
             throw new IndexOutOfBoundsException();
         }
     }
@@ -81,14 +107,18 @@ public class Percolation {
     }
 
     private int calculateIndex(int row, int col) {
-        return (row * size) - (size - col);
+        return (row * n) - (n - col);
     }
 
     public static void main(String[] args) {
-        Percolation p = new Percolation(100);
+        Percolation p = new Percolation(4);
         boolean isOpen = p.isOpen(1, 1);
-        System.out.println("isOpen: " + isOpen);
-        System.out.println(p.percolates());
+//        System.out.println("isOpen: " + isOpen);
+        int row = 1;
+        int col = 3;
+        System.out.println("Size: " + p.size + " n: " + p.n);
+        System.out.println("Index: " + p.calculateIndex(row, col));
+        System.out.println("Position: " + p.findPosition(row, col));
     }
 
 }

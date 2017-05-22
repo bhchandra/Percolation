1bcffbba66e6fe6928eb41fe0fb92546f829a1cf
Mitra <bhanumchandra@gmail.com>
2017-05-21 19:15:10 -0500

    Moving classes to default package confirming to the submission specification.

---
+++ src/main/java/PercolationStats.java
@@ -0,0 +1,89 @@
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+/**
+ *
+ * @author MITRA
+ */
+public class PercolationStats {
+
+    private final int n;
+    private final int trials;
+    private final double percolationResults[];
+
+    // perform trials independent experiments on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        this.n = n;
+        this.trials = trials;
+        percolationResults = new double[trials];
+        performTrials(trials);
+    }
+
+    private void performTrials(int trials) {
+        for (int i = 0; i < trials; i++) {
+            Percolation p = new Percolation(n);
+            while (!p.percolates()) {
+                int row = StdRandom.uniform(1, n + 1);
+                int col = StdRandom.uniform(1, n + 1);
+                p.open(row, col);
+            }
+            double percolation = (p.numberOfOpenSites() * 1.0) / (n * n);
+            percolationResults[i] = percolation;
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(percolationResults);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(percolationResults);
+
+    }
+
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - confidence();
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return mean() + confidence();
+    }
+
+    private double confidence() {
+        double s = stddev();
+        double sqrtTrials = Math.sqrt(trials);
+        return 1.96 * s / sqrtTrials;
+    }
+
+    public static void main(String[] args) {
+        int n = 0;
+        int trials = 0;
+        if (args.length > 0) {
+            try {
+                n = Integer.parseInt(args[0]);
+                trials = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                System.err.println("Arguments must be valid integers.");
+                System.exit(1);
+            }
+        }
+        if (args.length == 0) {
+            System.err.println("Please enter arguments to run the program.");
+            System.err.println("Exiting.");
+            System.exit(1);
+        }
+
+        final PercolationStats ps = new PercolationStats(n, trials);
+
+        StdOut.printf("mean                    = %s\n", ps.mean());
+        StdOut.printf("stddev                  = %s\n", ps.stddev());
+        StdOut.println("95% confidence interval = [" + ps.confidenceLo() + ", " + ps.confidenceHi() + "]");
+
+    }
+}
